2492a1d9fcd0e507a0b250821e2c5fc0cb8c3c84
NurlanNuruzada <nurlangn@code.edu.az>
2023-08-11 23:06:53 +0400

    ass

--- Query-React/fiorella-app/src/Services/CategoryService.js
+++
@@ -1,7 +0,0 @@
-import { httpClient } from '../utils/HttpClient';
-export const getCategories = () =>{
-  return httpClient.get("/api/Catagories")
-};
-export const PostCategories = (data) =>{
-  return httpClient.post("/api/Catagories",data)
-};
